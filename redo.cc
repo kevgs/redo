cdbfb9048b6e9d8b4128b7ac04a80a611f0dc183
Eugene Kosov <claprix@yandex.ru>
2020-03-24 22:51:43 +0300

    add LLFIO

--- redo.cc
+++ redo.cc
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <fmt/format.h>
+#include <llfio.hpp>
+
+namespace llfio = LLFIO_V2_NAMESPACE;
+
+
+int main() { fmt::print("Hello world\n");
 
 
 
-int main() { fmt::print("Hello world\n"); }
+  llfio::file_handle fh = llfio::file(  //
+    {},       // path_handle to base directory
+    "redo"    // path_view to path fragment relative to base directory
+              // default mode is read only
+              // default creation is open existing
+              // default caching is all
+              // default flags is none
+  ).value();  // If failed, throw a filesystem_error exception
+}
