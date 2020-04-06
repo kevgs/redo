8c0cdc4dd9783f4a627f9b115df0faef1a9ea47f
Eugene Kosov <claprix@yandex.ru>
2020-04-06 18:09:39 +0300

    add write_all() function

--- redo.cc
+++ redo.cc
@@ -14,9 +14,50 @@
 #include "redo/stop_token.hpp"
 
 namespace llfio = LLFIO_V2_NAMESPACE;
+using llfio::io_handle;
 
 static const llfio::file_handle::extent_type kFileSize = 128 * 1024 * 1024;
 static const char kFileName[] = "circular_file";
+
+void write_all(
+    io_handle &fd, llfio::file_handle::extent_type offset,
+    std::initializer_list<io_handle::const_buffer_type> lst) noexcept {
+  const static size_t kMaxBuffers = 64;
+
+  assert(std::distance(lst.begin(), lst.end()) <= kMaxBuffers);
+
+  std::array<io_handle::const_buffer_type, kMaxBuffers> buffer;
+  std::copy(lst.begin(), lst.end(), buffer.begin());
+
+  io_handle::const_buffers_type write_me{
+      buffer.data(),
+      static_cast<size_t>(std::distance(lst.begin(), lst.end()))};
+
+  do {
+    io_handle::io_request<io_handle::const_buffers_type> reqs{write_me, offset};
+    auto written_buffers = llfio::write(fd, reqs).value();
+
+    offset += std::accumulate(written_buffers.begin(), written_buffers.end(),
+                              static_cast<size_t>(0),
+                              [](auto a, auto b) { return a + b.size(); });
+
+    auto [_, it] =
+        std::mismatch(write_me.begin(), write_me.end(), written_buffers.begin(),
+                      [](auto a, auto b) { return a.size() == b.size(); });
+
+    // forget fully written buffers
+    write_me = write_me.subspan(std::distance(written_buffers.begin(), it));
+
+    // shrink partially written buffer
+    if (it != written_buffers.end()) {
+      assert(std::next(it) == written_buffers.end());
+
+      write_me.front() = {write_me.front().data() + it->size(),
+                          write_me.front().size() - it->size()};
+    }
+
+  } while (!write_me.empty());
+}
 
 class CircularFile {
 public:
@@ -32,7 +73,7 @@ public:
     buf.fill(std::byte{0});
 
     for (size_t i = 0; i < kFileSize; i += buf.size())
-      llfio::write(fh_, i, {{buf.data(), buf.size()}}).value();
+      write_all(fh_, i, {{buf.data(), buf.size()}});
   }
 
   ~CircularFile() noexcept { llfio::unlink(fh_).value(); }
@@ -42,13 +83,13 @@ public:
 
     if (offset_ + buf.size() > size_) {
       auto partial_size = size_ - offset_;
-      llfio::write(fh_, offset_, {{buf.data(), partial_size}}).value();
+      write_all(fh_, offset_, {{buf.data(), partial_size}});
       buf = llfio::io_handle::const_buffer_type(buf.data() + partial_size,
                                                 buf.size() - partial_size);
       offset_ = 0;
     }
 
-    llfio::write(fh_, offset_, {buf}).value();
+    write(fh_, offset_, {buf});
     offset_ = (offset_ + buf.size()) % size_;
 
     appends_performed_++;
